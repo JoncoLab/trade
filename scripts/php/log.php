15a27b7920b892958216ed2515a65aa26b0bfece
JoncoLab <joncolab@gmail.com>
2016-12-19 03:58:43 +0200

    1.2.1

--- scripts/php/log.php
+++ scripts/php/log.php
@@ -8,11 +8,7 @@
 session_start();
 include('user.php');
 
-if ($connection->connect_error) {
-    die ('Не вдається встановити підключення до бази даних:\r\n' . $connection->connect_error);
-} else {
-    $email = trim($_POST["login"]);
-    $user = User::getUserByEmail($email);
-    $_SESSION["id"] = $user->id;
-    header("Location: /cabinet-page.php");
-}
+$email = trim($_POST["login"]);
+$user = User::getUserByEmail($email);
+$_SESSION["id"] = $user->id;
+header("Location: /cabinet-page.php");
