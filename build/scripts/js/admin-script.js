c14ecf7445176d5306b516cd4958e2ddf31a3b66
JoncoLab <joncolab@gmail.com>
2016-12-20 15:09:55 +0200

    1.2.9

--- build/scripts/js/admin-script.js
+++ build/scripts/js/admin-script.js
@@ -3,44 +3,38 @@ var main;
 main = function () {
     $('.page-maker').hide();
     $('.write-article.page-maker').show();
-
     $('table').DataTable({
         "iDisplayLength": 25
     });
-
     $('header .navbar button').click(function () {
         $('.page-maker').hide();
-
         var classId = $(this).attr('id');
         $('.' + classId).toggle();
         if (classId === 'users') {
             $('#DataTables_Table_0_wrapper').show(0);
-        } else {
+        }
+        else {
             $('#DataTables_Table_0_wrapper').hide(0);
         }
     });
-
     $('form .content .add-paragr').click(function () {
         var paragraph = $('.content textarea').val();
         if (paragraph !== '') {
-            $('.content').append('<div class="new-paragr"><button class="remove-paragr" type="button">Стерти</button><button class="edit-paragr" type="button">Редаг</button><button type="button" class="edited">OK</button><p>' + paragraph + '</p></div>');
+            $('.content').append('<div class="new-paragr"><button class="remove-paragr" type="button">Стерти</button><button class="edit-paragr" type="button">Редаг</button><button type="button" class="edited">OK</button><p class="paragraph">' + paragraph + '</p></div>');
             $('.content > textarea').val('');
         }
-
         $('.clear').click(function () {
-            $('.content textarea').val('');
+            $('.content >textarea').val('');
         });
-
-        $('.content .new-paragr .edit-paragr').click(function () {
-            var editStuff = $(this).siblings('p').val();
+        $('.edit-paragr').unbind().on('click', function () {
+            var editStuff = $(this).siblings('p').html();
             $(this).siblings('textarea').remove();
+            $(this).parent().prepend('<textarea autofocus rows="12" tabindex="1">' + editStuff + '</textarea>');
             $(this).siblings('p').remove();
             $(this).hide();
             $(this).siblings('.edited').show();
-            $(this).parent('.new-paragr').prepend('<textarea autofocus rows="12" tabindex="1">' + editStuff + '</textarea>');
             $(this).siblings('textarea').focus();
         });
-
         $('.content .new-paragr .edited').click(function () {
             var editedParagr = $(this).siblings('textarea').text();
             if (editedParagr !== '') {
@@ -51,24 +45,30 @@ main = function () {
                 $(this).siblings('.edit-paragr').show();
             }
         });
-
         $('.content .new-paragr .remove-paragr').click(function () {
             $(this).parent().remove();
         });
-
         $('.watch').on("click", function () {
-            $('article.preview').remove();
-            if ($('#header').val() !== '' ) {
-               var header = "<h2>" + $('#header').val() + "</h2>";
+            var header = "<h2>" + $('#header').val() + "</h2>";
+            var p = "";
+            $('.new-paragr').each(function () {
+                p = p + "<p>" + $(this).children('p').html() + "</p>";
+            });
+            if ($('.new-paragr').length !== 0 && $('#header').val() !== '') {
+                var article = "<article class='preview'> <span class='back' style='color: grey; float: right;'>повернутись - подвійний клік по статті</span>" + header + p + "</article>";
+                $('fieldset').hide();
+                $('article.preview').remove();
+                $('form.write-article').append(article);
             }
-            if ($('.new-paragr').length !== 0) {
-                var p = "";
-                $('.new-paragr').each(function () {
-                    p = p + "<p>" + $(this).children('p').html() + "</p>";
-                });
+            else if ($('#header').val() == '') {
+                $('#header').toggleClass('error');
+                return false;
             }
-            var article = "<article class='preview'>" + header + p + "</article>";
-            $('form.write-article').append(article);
+            $('article').focus();
+            $('article.preview').dblclick(function () {
+                $(this).remove();
+                $('fieldset').show();
+            })
         });
     });
 };
